dea284f8a6f0f0881e0c0c33298d6f1a0f58dc61
Danylo Zaviryukha <d.zaviryukha@astoundcommerce.com>
2018-05-15 17:59:05 +0300

    start

---
+++ dist/let.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var buttons = document.querySelectorAll('button');
+console.log(buttons);
+
+var _loop = function _loop(i) {
+    button = buttons[i];
+
+    button.innerText = i;
+    button.onclick = function (e) {
+        console.log(i);
+    };
+};
+
+for (var i = 0; i < buttons.length; i++) {
+    var button;
+
+    _loop(i);
+}
